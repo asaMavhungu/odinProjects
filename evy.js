11766be3e4eb4f1c728eb3951c508a242134da50
Asakundwi Mavhungu <mvhasa001@sl-dual-205.cs.uct.ac.za>
2022-08-08 16:07:06 +0200

    button changes color on click

--- evy.js
+++ evy.js
@@ -11,8 +11,7 @@ function asa(){
 const ten = () => alert("Bye");
 
 const element = document.querySelector("#btn1");
-element.addEventListener('click', (event) => {
-  console.log(event.target.value);
-  alert("asa");
+element.addEventListener('click', (ef) => {
+  ef.target.style.background = 'blue';
   // `event` is passed into the callback from the `.addEventListener` function when it receives a 'click' event.
 });
